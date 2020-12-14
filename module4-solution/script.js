3ffd5437858da17238b22f57d43504443bd0c4b6
Kartik Soni <kattosoni@gmail.com>
2020-12-14 15:38:22 +0530

    module 4 assignment

---
+++ module4-solution/script.js
@@ -0,0 +1,13 @@
+(function (){
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for (var n in names)
+{
+  var firstLetter = names[n].charAt(0);;
+  firstLetter = firstLetter.toLowerCase(); 
+  if (firstLetter == 'j') {
+     byeSpeaker.speak(names[n]);
+  } else {
+   helloSpeaker.speak(names[n]);
+  }
+}
+})();
